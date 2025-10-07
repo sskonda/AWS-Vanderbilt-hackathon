f4ce4e2868464d5ce0b119b161594d5cdee6eca7
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2025-10-07 01:15:28 -0400

    Add Python package

---
+++ ros_ws/src/uuv_sim_py/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'uuv_sim_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kapow',
+    maintainer_email='iyer.h.nikhil@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
